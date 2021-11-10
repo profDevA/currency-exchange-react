99e045841bebb17c370dc5b040eff7f83fdf7f2a
profDevA <toptaldev123@gmail.com>
2021-11-11 01:31:55 +0800

    refactor: change API service

--- src/api/getRates.js
+++ src/api/getRates.js
@@ -1,14 +1,17 @@
-const baseURL = 'http://api.exchangeratesapi.io/v1/latest';
+const baseURL = 'https://v6.exchangerate-api.com/v6/';
 const accessKey = process.env.REACT_APP_ACCESS_KEY;
 
 export const getRates = async () => {
-  let response = await fetch(
-    `${baseURL}?access_key=${accessKey}&symbols=USD,EUR,GBP`
-  );
+  let response = await fetch(`${baseURL}${accessKey}/latest/EUR`);
 
   if (response.ok) {
     let json = await response.json();
-    return json.rates;
+    const rates = {
+      EUR: json.conversion_rates.EUR,
+      USD: json.conversion_rates.USD,
+      GBP: json.conversion_rates.GBP,
+    };
+    return rates;
   } else {
     console.log('Error HTTP: ' + response.status);
   }
