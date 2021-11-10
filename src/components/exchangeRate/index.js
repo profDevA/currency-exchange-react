3e7413c6844c26ade4db179f83fe19ae9455d7a2
profDevA <toptaldev123@gmail.com>
2021-11-10 23:29:18 +0800

    chore: setup eslint & prettier

--- src/components/exchangeRate/index.js
+++ src/components/exchangeRate/index.js
@@ -1,22 +1,22 @@
-import { useState, useEffect } from "react";
-import { MdOutlineSwapVerticalCircle } from "react-icons/md";
-import { toast } from "react-toastify";
-import ExchangeRateItem from "./exchangeRateItem";
-import { getRates } from "../../api/getRates";
-import { round, currencyFormat } from "../../utils";
+import { useState, useEffect } from 'react'
+import { MdOutlineSwapVerticalCircle } from 'react-icons/md'
+import { toast } from 'react-toastify'
+import ExchangeRateItem from './exchangeRateItem'
+import { getRates } from '../../api/getRates'
+import { round, currencyFormat } from '../../utils'
 
 export default function ExchangeRate() {
   const [balance, setBalance] = useState({
     USD: 200,
     EUR: 150,
     GBP: 10,
-  });
+  })
   const [state, setState] = useState({
-    fromCode: "USD",
+    fromCode: 'USD',
     fromAmount: 0,
-    toCode: "EUR",
+    toCode: 'EUR',
     toAmount: 0,
-  });
+  })
   const [rates, setRates] = useState({
     USDEUR: 0.8626,
     USDGBP: 0.73745,
@@ -24,7 +24,7 @@ export default function ExchangeRate() {
     EURGBP: 0.854915,
     GBPUSD: 1.356025,
     GBPEUR: 1.169707,
-  });
+  })
 
   // Get the exchange rate on first load. Because API data is updated daily,
   // there is no need to fetch exchange rates in real time (Also free plan does not support it).
@@ -40,93 +40,93 @@ export default function ExchangeRate() {
     //     GBPEUR: round(rates.EUR / rates.GBP, 4),
     //   });
     // });
-  }, []);
+  }, [])
 
   // warn if fromAmount is grater than fromBalance
   useEffect(() => {
     if (state.fromAmount > balance[state.fromCode]) {
-      toast.warn("Exceeds balance");
+      toast.warn('Exceeds balance')
     }
-  }, [state, balance]);
+  }, [state, balance])
 
   useEffect(() => {
-    const { fromAmount, fromCode, toCode } = state;
+    const { fromAmount, fromCode, toCode } = state
     if (fromAmount > 0) {
       setState({
         ...state,
         toAmount: round(fromAmount * rates[fromCode + toCode]),
-      });
+      })
     }
-  }, [state.fromCode, state.toCode]);
+  }, [state.fromCode, state.toCode])
 
   const exchangeCurrency = () => {
-    const { fromCode, toCode, fromAmount, toAmount } = state;
+    const { fromCode, toCode, fromAmount, toAmount } = state
 
     // Warn if currency is not selected
     if (fromCode === toCode) {
-      toast.info("Select a currency to exchange");
-      return;
+      toast.info('Select a currency to exchange')
+      return
     }
 
     // Warn if amount to exchange is 0
     if (!fromAmount) {
-      toast.info("Input amount to exchange");
-      return;
+      toast.info('Input amount to exchange')
+      return
     }
 
     // Warn if balance is not enough
     if (fromAmount > balance[fromCode]) {
-      toast.error("Can't exchange. Balance is not enough.");
-      return;
+      toast.error("Can't exchange. Balance is not enough.")
+      return
     }
     // fromBalance = frombalance - fromamount
     setBalance({
       ...balance,
       [fromCode]: round(balance[fromCode] - fromAmount, 2),
       [toCode]: round(balance[toCode] + toAmount, 2),
-    });
+    })
 
-    clearExchangeAmount();
+    clearExchangeAmount()
 
-    toast.success("Successfully exchanged!");
-  };
+    toast.success('Successfully exchanged!')
+  }
 
   const clearExchangeAmount = () => {
-    setState({ ...state, fromAmount: 0, toAmount: 0 });
-  };
+    setState({ ...state, fromAmount: 0, toAmount: 0 })
+  }
 
   const handleCode = (type, code) => {
-    if (type === "quote") {
-      setState({ ...state, fromCode: code });
+    if (type === 'quote') {
+      setState({ ...state, fromCode: code })
     } else {
-      setState({ ...state, toCode: code });
+      setState({ ...state, toCode: code })
     }
-  };
+  }
 
   const handleInput = (type, amount) => {
-    const { fromCode, toCode } = state;
+    const { fromCode, toCode } = state
 
-    if (type === "quote") {
+    if (type === 'quote') {
       setState({
         ...state,
         fromAmount: amount,
         toAmount: round(amount * rates[fromCode + toCode], 2),
-      });
+      })
     } else {
       setState({
         ...state,
         fromAmount: round(amount / rates[fromCode + toCode], 2),
         toAmount: amount,
-      });
+      })
     }
-  };
+  }
 
-  const baseStr = currencyFormat(1, state.fromCode, 0);
+  const baseStr = currencyFormat(1, state.fromCode, 0)
 
   const convertStr =
     state.fromCode === state.toCode
       ? currencyFormat(1, state.fromCode, 0)
-      : currencyFormat(rates[state.fromCode + state.toCode], state.toCode, 4);
+      : currencyFormat(rates[state.fromCode + state.toCode], state.toCode, 4)
 
   return (
     <>
@@ -150,7 +150,7 @@ export default function ExchangeRate() {
           />
         </div>
         <span className="rate">
-          {baseStr} = <span>{!convertStr.includes("NaN") && convertStr}</span>
+          {baseStr} = <span>{!convertStr.includes('NaN') && convertStr}</span>
         </span>
         <i className="swap-icon">
           <MdOutlineSwapVerticalCircle color="green" />
@@ -160,5 +160,5 @@ export default function ExchangeRate() {
         <button onClick={exchangeCurrency}>Exchange</button>
       </div>
     </>
-  );
+  )
 }
